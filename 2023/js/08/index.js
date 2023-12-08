6c983a7e363dd67697aa3b61eab1b0033236345e
pritom <iamsakibshadmanpritom@gmail.com>
2023-12-08 22:10:54 +0600

    update 08 js

--- 2023/js/08/index.js
+++ 2023/js/08/index.js
@@ -1,4 +1,3 @@
-// 8.19
 const fs = require("fs");
 const path = require("path");
 const { lcm } = require("./find_lcm");
@@ -7,9 +6,6 @@ const { lcm } = require("./find_lcm");
 // const FILE_NAME = "../../data/08/ex.txt";
 const FILE_NAME = "../../data/08/data.txt";
 
-// @for debugging
-// const FILE_NAME = "./data/08/ex2.txt";
-// const FILE_NAME = "./data/08/data.txt";
 const abs_path = path.resolve(FILE_NAME);
 const INPUT = fs.readFileSync(abs_path, "ascii").trim();
 
@@ -30,6 +26,7 @@ class Graph {
     this.nodes[name] = new Node(name, left, right);
   }
 
+  // dfs of graph
   findItem(start, destination, walk_pattern) {
     let pattern_index = 0;
     let steps = 0;
@@ -55,6 +52,8 @@ class Graph {
     return steps;
   }
 
+  // dfs and get step for every single node
+  // returning individual steps arr
   findItem_03(start_nodes, destination, walk_pattern) {
     const nodes_arr = this.getGraphNodes(start_nodes);
     const steps_arr = [];
@@ -144,6 +143,7 @@ class Graph {
     return x;
   }
 
+  //
   findItem_02(start_nodes, destination, walk_pattern) {
     const nodes_arr = this.getGraphNodes(start_nodes);
 
@@ -182,6 +182,8 @@ class Graph {
   }
 }
 
+// ===============================================
+
 const start_node_names = [];
 
 function parseInput(input) {
@@ -216,12 +218,12 @@ function parseInput(input) {
   return graph;
 }
 
-// const r = g.findItem("AAA", "ZZZ", REPEAT_PATTERN);
-// const steps_arr = g.findItem_03(start_node_names, "Z", REPEAT_PATTERN);
-
-function part_1() {
-  return;
+function part_1(input) {
+  const g = parseInput(input);
+  const result = g.findItem("AAA", "ZZZ", REPEAT_PATTERN);
+  console.log(result);
 }
+
 function part_2(input) {
   const g = parseInput(input);
   const steps_arr = g.findItem_03(start_node_names, "Z", REPEAT_PATTERN);
@@ -230,4 +232,5 @@ function part_2(input) {
   console.log(result);
 }
 
-part_2(INPUT);
+// part_1(INPUT);
+// part_2(INPUT);
