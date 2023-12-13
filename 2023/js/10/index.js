8a98f3933fa97136f52dd6b098fd6d9dadd0b716
pritom <pritom@pop-os.localdomain>
2023-12-13 12:29:26 +0600

    2023/10js

--- 2023/js/10/index.js
+++ 2023/js/10/index.js
@@ -1,19 +1,24 @@
-const input = `-L|F7
-7S-7|
-L|7||
--L-J|
-L|-JF`;
+const fs = require("fs");
+
+// const input = `-L|F7
+// 7S-7|
+// L|7||
+// -L-J|
+// L|-JF`;
+
+// const input = fs.readFileSync("./ex.txt", "ascii");
+// const input = fs.readFileSync("./ex1.txt", "ascii");
+const input = fs.readFileSync("./data.txt", "ascii");
 
 const DIR = [
   [-1, 0],
+  [0, 1],
   [1, 0],
   [0, -1],
-  [0, 1],
 ];
 
 const VALID_NEXT_DIR = {
-  S: [0, 1, 2, 3, 4],
-  L: [0, 3],
+  L: [],
   J: [0, 2],
   7: [1, 2],
   F: [1, 3],
@@ -21,18 +26,80 @@ const VALID_NEXT_DIR = {
   "-": [2, 3],
 };
 
+const EXPECTED_NEXT_PATH = {
+  0: ["|", "F", "7"],
+  1: ["-", "7", "J"],
+  2: ["|", "J", "L"],
+  3: ["-", "F", "L"],
+};
+
+const isValidObj = {
+  L: { 0: ["|", "7", "F"], 1: ["J", "7", "-"], 2: [], 3: [] },
+  J: { 0: ["|", "7", "F"], 1: [], 2: [], 3: ["-", "F", "L"] },
+  7: { 0: [], 1: [], 2: ["|", "J", "L"], 3: ["-", "F", "L"] },
+  F: { 0: [], 1: ["J", "7", "-"], 2: ["|", "J", "L"], 3: [] },
+  "|": { 0: ["|", "7", "F"], 1: [], 2: ["|", "J", "L"], 3: [] },
+  "-": { 0: [], 1: ["J", "7", "-"], 2: [], 3: ["-", "F", "L"] },
+  S: {
+    0: ["|", "7", "F"],
+    1: ["J", "7", "-"],
+    2: ["|", "J", "L"],
+    3: ["-", "F", "L"],
+  },
+};
+
+/**
+ *
+ * @param {string} curr character
+ * @param {string} next character
+ * @param {string} dir 0 | 1 | 2 | 3 css order
+ * @returns
+ */
+function isValid(curr, prev, dir) {
+  // console.log("curr: ", curr, "prev: ", prev, "dir: ", dir);
+  if (curr === ".") {
+    return false;
+  }
+  if (curr === "S") {
+    return true;
+  }
+  if (isValidObj[prev][dir].includes(curr)) {
+    return true;
+  }
+
+  return false;
+}
+
 const maze_grid = input
   .trim()
   .split("\n")
   .map((item) => item.trim())
   .map((item) => item.split(""));
 
-const START = [1, 1];
+let START;
+
+for (let [lx, line] of maze_grid.entries()) {
+  for (let [cx, ch] of line.entries()) {
+    if (ch === "S") {
+      START = [lx, cx];
+    }
+  }
+}
 
 // console.log(maze_grid);
+function getNextValidPath(curr_idx) {
+  switch (curr_idx) {
+    case "0":
+      break;
 
-function walk(curr, des, grid, isFirst, seen, path, valid_path) {
+    default:
+      break;
+  }
+}
+
+function walk(curr, prev, dir, des, grid, isFirst, seen, path) {
   if (curr === des && !isFirst) {
+    path.push([curr[0], curr[1]]);
     return true;
   }
 
@@ -49,46 +116,33 @@ function walk(curr, des, grid, isFirst, seen, path, valid_path) {
     return false;
   }
 
+  const curr_ch = grid[curr[0]][curr[1]];
+  if (!isValid(curr_ch, prev, dir)) {
+    return false;
+  }
+
   seen.push(`${curr[0]} ${curr[1]}`);
   path.push([curr[0], curr[1]]);
 
-  const curr_ch = grid[curr[0]][curr[1]];
-
-  for (let item of VALID_NEXT_DIR[curr_ch]) {
+  for (let item = 0; item < DIR.length; item++) {
     const new_x = curr[0] + DIR[item][0];
     const new_y = curr[1] + DIR[item][1];
 
-    let valid_path;
-
-    switch (item) {
-      case 0:
-        valid_path = 1;
-        break;
-      case 1:
-        valid_path = 0;
-      case 2:
-        valid_path = 3;
-      case 3:
-        valid_path = 2;
-      default:
-        break;
-    }
-
-    if (walk([new_x, new_y], des, grid, false, seen, path)) {
+    if (walk([new_x, new_y], curr_ch, item, des, grid, false, seen, path)) {
       return true;
     }
   }
-
-  path.pop();
+  // path.pop();
 }
 
 function main() {
   const seen = [];
   const path = [];
 
-  walk(START, START, maze_grid, true, seen, path, 1);
+  walk(START, null, null, START, maze_grid, true, seen, path);
 
   console.log(path);
+  console.log(path.length / 2);
 }
 
 main();
