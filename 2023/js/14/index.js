781465f94e1577d38dfc6a57e33b29f70fab8b6e
pritom <iamsakibshadmanpritom@gmail.com>
2023-12-14 18:23:00 +0600

    2023/14js

--- 2023/js/14/index.js
+++ 2023/js/14/index.js
@@ -1,90 +1,91 @@
 /**
  * part 1done
- * part 2test done
+ * part 2done
  */
 import fs from "fs";
 const INPUT_GRID = fs
-  .readFileSync("./ex.txt", "ascii")
-  // .readFileSync("./data.txt", "ascii")
-  .trim()
-  .split("\n")
-  .map((item) => item.split(""));
+	// .readFileSync("./ex.txt", "ascii")
+	.readFileSync("./data.txt", "ascii")
+	.trim()
+	.split("\n")
+	.map((item) => item.split(""));
 
 class Grid {
-  OBSTACLES = ["#", "O"];
-  CYCLES = 3;
+	OBSTACLES = ["#", "O"];
 
-  constructor(input_grid) {
-    this.input_grid = input_grid;
+	// input how many cycles to run
+	CYCLES = +process.argv[2];
 
-    this.turn(this.CYCLES);
-    this.logger();
-  }
+	constructor(input_grid) {
+		this.input_grid = input_grid;
 
-  tilt() {
-    let changed = true;
-    while (changed) {
-      changed = false;
-      for (let line = 1; line < this.input_grid.length; line++) {
-        for (let ch = 0; ch < this.input_grid[0].length; ch++) {
-          if (
-            this.input_grid[line][ch] === "O" &&
-            !this.OBSTACLES.includes(this.input_grid[line - 1][ch])
-          ) {
-            this.input_grid[line - 1][ch] = "O";
-            this.input_grid[line][ch] = ".";
-            changed = true;
-          }
-        }
-      }
-    }
-  }
+		this.turn(this.CYCLES);
+		this.findTotalLoad();
+	}
 
-  turn(cycles) {
-    for (let i = 0; i < cycles; i++) {
-      for (let j = 0; j < 4; j++) {
-        this.tilt();
-        this.rotate();
-        this.logger();
-      }
-    }
-  }
+	tilt() {
+		let changed = true;
+		while (changed) {
+			changed = false;
+			for (let line = 1; line < this.input_grid.length; line++) {
+				for (let ch = 0; ch < this.input_grid[0].length; ch++) {
+					if (
+						this.input_grid[line][ch] === "O" &&
+						!this.OBSTACLES.includes(this.input_grid[line - 1][ch])
+					) {
+						this.input_grid[line - 1][ch] = "O";
+						this.input_grid[line][ch] = ".";
+						changed = true;
+					}
+				}
+			}
+		}
+	}
 
-  rotate() {
-    const new_arr = [];
-    this.input_grid[0].forEach((item) => new_arr.push([]));
+	turn(cycles) {
+		for (let i = 1; i <= cycles; i++) {
+			for (let j = 0; j < 4; j++) {
+				this.tilt();
+				this.rotate();
+			}
+		}
+	}
 
-    this.input_grid.forEach((line, lx) => {
-      line.forEach((ch, cx) => {
-        new_arr[cx].unshift(ch);
-      });
-    });
+	rotate() {
+		const new_arr = [];
+		this.input_grid[0].forEach((_) => new_arr.push([]));
 
-    this.input_grid = new_arr;
-  }
+		this.input_grid.forEach((line) => {
+			line.forEach((ch, cx) => {
+				new_arr[cx].unshift(ch);
+			});
+		});
 
-  findTotalLoad() {
-    let total = 0;
+		this.input_grid = new_arr;
+	}
 
-    this.input_grid.forEach((line, lx) => {
-      const nos_o = line.reduce((acc, curr) => {
-        if (curr === "O") {
-          return (acc = acc + 1);
-        }
-        return acc;
-      }, 0);
-      total += (this.input_grid.length - lx) * nos_o;
-      console.log(total);
-    });
-    console.log(total);
-  }
+	findTotalLoad() {
+		let total = 0;
 
-  logger() {
-    this.input_grid.forEach((item) => {
-      console.log(item.join(""));
-    });
-    console.log("\n");
-  }
+		this.input_grid.forEach((line, lx) => {
+			const nos_o = line.reduce((acc, curr) => {
+				if (curr === "O") {
+					return (acc = acc + 1);
+				}
+				return acc;
+			}, 0);
+			total += (this.input_grid.length - lx) * nos_o;
+		});
+		console.log(total);
+		return total;
+	}
+
+	logger() {
+		this.input_grid.forEach((item) => {
+			console.log(item.join(""));
+		});
+		console.log("\n");
+	}
 }
 
 const grid = new Grid(INPUT_GRID);
