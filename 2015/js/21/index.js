d418a60b864fef771cb002c38d35aba223ed8604
pritom <pritom@pop-os.localdomain>
2023-12-14 09:17:51 +0600

    2015/21js

---
+++ 2015/js/21/index.js
@@ -0,0 +1,9 @@
+// make Battle function
+/// while player and boss alive
+//// players shoots
+
+//// if boss alive
+//// boss shoots
+
+// make an array of all the winner players
+// check to find sample in the winner players list
