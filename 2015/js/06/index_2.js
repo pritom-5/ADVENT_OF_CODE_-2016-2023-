69b532c0bc312ef3042f00eb6bcfc5cdd5ba6847
pritom <pritom@pop-os.localdomain>
2023-12-17 13:07:43 +0600

    *

--- 2015/js/06/index_2.js
+++ 2015/js/06/index_2.js
@@ -3,27 +3,42 @@ class LightGrid {
   MAX_ROW = 10;
   lights = {};
 
-  constructor() {}
+  constructor() {
+    this.#populateLights();
+  }
+
+  #populateLights() {
+    for (let i = 0; i < this.MAX_ROW; i++) {
+      this.lights[i] = [];
+    }
+  }
 
   /**
    * @param {[number, number]} from
    * @param {[number, number]} to
    */
   add(from, to) {
-    for (let r = from[1]; r <= to[1]; r++) {
-      const col_limit = to[1] === r ? to[1] : this.MAX_COL;
-      const col_start = from[1] === r ? from[1] : 0;
-      for (let c = col_start; c <= col_limit; c++) {
-        if (this.lights[r]) {
-          if (!this.lights[r].includes(c)) {
-            this.lights[r][c] = 1;
-          }
-        } else {
-          this.lights[r] = new Array(this.MAX_COL).fill(0);
+    const row_start = from[1];
+    const row_end = to[1];
+    const col_start = from[0];
+    const col_end = to[0];
+
+    for (let r = row_start; r <= row_end; r++) {
+      const min_col = row_start === r ? col_start : 0;
+      const max_col = row_end === r ? col_end : this.MAX_COL;
+
+      for (let c = min_col; c <= max_col; c++) {
+        if (!this.lights[r].includes(c)) {
+          this.lights[r].push(c);
         }
       }
+
       this.#sortArr(r);
     }
+  }
+
+  #filterOutInvalidLights(row_idx) {
+    this.lights[row_idx] = this.lights[row_idx].filter((item) => item !== -1);
   }
 
   /**
@@ -32,11 +47,44 @@ class LightGrid {
    */
   remove(from, to) {
     for (let r = from[1]; r <= to[1]; r++) {
-      const col_limit = to[1] === r ? to[1] : this.MAX_COL;
-      const col_start = from[1] === r ? from[1] : 0;
+      const row_start = from[1];
+      const row_end = to[1];
+      const col_start = from[0];
+      const col_end = to[0];
+
+      const min_col = row_start === r ? col_start : 0;
+      const max_col = row_end === r ? col_end : this.MAX_COL;
+
+      for (let j = 0; j <= this.lights[r].length; j++) {
+        if (this.lights[r][j] >= min_col && this.lights[r][j] <= max_col) {
+          this.lights[r][j] = -1;
+        }
+      }
+
+      this.#filterOutInvalidLights(r);
+    }
+  }
+
+  toggle(from, to) {
+    const row_start = from[1];
+    const row_end = to[1];
+    const col_start = from[0];
+    const col_end = to[0];
+
+    for (let r = row_start; r <= row_end; r++) {
+      const min_col = row_start === r ? col_start : 0;
+      const max_col = row_end === r ? col_end : this.MAX_COL;
+
+      for (let c = min_col; c <= max_col; c++) {
+        if (!this.lights[r].includes(c)) {
+          this.lights[r].push(c);
+        }
+      }
 
       this.#sortArr(r);
     }
+
+    this.#filterOutInvalidLights(r);
   }
 
   #sortArr(row_idx) {
@@ -48,8 +96,11 @@ class LightGrid {
   }
 }
 
+// [x, y] [x1, y1]
 const a = new LightGrid();
 a.add([2, 3], [4, 4]);
 a.add([0, 3], [9, 4]);
+
+a.remove([4, 3], [5, 4]);
 
 a.print();
